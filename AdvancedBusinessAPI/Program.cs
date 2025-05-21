9ab6c9623ae34ca7289bdefe3e1e8249d2bf3316
Gustavo Camargo <161590899+tcguus@users.noreply.github.com>
2025-05-21 09:22:45 -0300

    Update.

--- AdvancedBusinessAPI/Program.cs
+++ AdvancedBusinessAPI/Program.cs
@@ -3,14 +3,10 @@ using AdvancedBusinessAPI.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Adiciona o DbContext com Oracle
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseOracle(builder.Configuration.GetConnectionString("OracleConnection")));
 
-// Adiciona suporte a controllers
 builder.Services.AddControllers();
-
-// Adiciona Swagger
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
